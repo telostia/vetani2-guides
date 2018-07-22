674059d42a21c5f2ceaa5e2b1bf5e87b81ee51a5
telostia <telostia@gmail.com>
2018-07-22 11:44:48 +1000

    refix guides

--- guides/vetani_auto.sh
+++ guides/vetani_auto.sh
@@ -24,8 +24,6 @@ chmod +x vetani*
 cp vetani* /usr/local/bin
 ufw allow 1977/tcp
 
-
-
 #masternode input
 
 echo -e "${GREEN}Now paste your Masternode key by using right mouse click and press ENTER ${NONE}";
@@ -35,9 +33,11 @@ EXTIP=`wget -qO- eth0.me`
 PASSW=`pwgen -1 20 -n`
 
 echo -e "${GREEN}Preparing config file ${NONE}";
-sudo mkdir /root/.vetani
 
-printf "addnode=54.149.181.41:38221\naddnode=5.9.180.147:38221\naddnode=74.208.94.204:38221\naddnode=104.207.158.193:38221\naddnode=138.68.165.112:38221\naddnode=54.149.181.41:38221\naddnode=95.179.140.249:38221\naddnode=104.237.149.64:38221\naddnode=185.233.105.172:38221\naddnode=45.76.146.140:38221\naddnode=185.53.191.78:38221\n\nrpcuser=vetani432345$PASSW\nrpcpassword=$PASSW\nrpcport=1977\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=256\nexternalip=$EXTIP:38221\nmasternode=1\nmasternodeprivkey=$MNKEY" >  /root/.vetani/vetani.conf
+rm -rf $HOME/.vetani
+sudo mkdir $HOME/.vetani
+
+printf "addnode=54.149.181.41:38221\naddnode=5.9.180.147:38221\naddnode=74.208.94.204:38221\naddnode=104.207.158.193:38221\naddnode=138.68.165.112:38221\naddnode=54.149.181.41:38221\naddnode=95.179.140.249:38221\naddnode=104.237.149.64:38221\naddnode=185.233.105.172:38221\naddnode=45.76.146.140:38221\naddnode=185.53.191.78:38221\n\nrpcuser=vetani432345$PASSW\nrpcpassword=$PASSW\nrpcport=1977\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=256\nexternalip=$EXTIP:38221\nmasternode=1\nmasternodeprivkey=$MNKEY" >  $HOME/.vetani/vetani.conf
 
 
 vetanid -daemon
